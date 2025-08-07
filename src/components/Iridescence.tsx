53166b75b2a5ed20c913aec76606bacafaaa1654
Eduardo Piazza <dudupiazza16@gmail.com>
2025-08-07 16:02:03 -0300

    Update Iridescence.tsx

--- src/components/Iridescence.tsx
+++ src/components/Iridescence.tsx
@@ -1,1 +1,9 @@
+import React from 'react';
+import './Iridescence.css';
 
+// Placeholder component for future iridescence effects
+const Iridescence: React.FC = () => {
+  return null;
+};
+
+export default Iridescence;
